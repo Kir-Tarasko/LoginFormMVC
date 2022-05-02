a000d4d1027af781eaab4f6cc2fb62e98fd0a1b1
Кирилл Тараско <tarasko.kirill@icloud.com>
2022-05-02 16:16:19 +0200

    Some fixes

--- LoginFormMVC/PersonalDataViewController.swift
+++ LoginFormMVC/PersonalDataViewController.swift
@@ -11,21 +11,7 @@ class PersonalDataViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
         title = human.description
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+ 
 }
